07f0674fdea976d91ab205f4958980b5d18eb5f9
gerald <geraldmontufar@gmail.com>
2025-05-21 15:15:48 +0200

    initial commit

---
+++ backend/src/test/java/com/geraldsaccount/mordine/MordineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldsaccount.mordine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MordineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
