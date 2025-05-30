cf1365d5427cae579a54084daac66eb7a250cabe
gerald <geraldmontufar@gmail.com>
2025-05-30 17:15:36 +0200

    updated user and user repository

--- backend/src/main/java/com/geraldsaccount/killinary/repository/UserRepository.java
+++ backend/src/main/java/com/geraldsaccount/killinary/repository/UserRepository.java
@@ -1,18 +1,16 @@
 package com.geraldsaccount.killinary.repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.geraldsaccount.killinary.model.User;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
 
     boolean existsByUsername(String username);
 
-    boolean existsByEmail(String email);
 }
