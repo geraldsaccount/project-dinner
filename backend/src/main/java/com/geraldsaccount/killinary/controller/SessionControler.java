632ddd385bb9b934efc3b5630dd5377146e787de
gerald <geraldmontufar@gmail.com>
2025-06-02 15:42:16 +0200

    added SessionSummaryDTO and endpoint to retrieve the sessionsummaries for a given user

---
+++ backend/src/main/java/com/geraldsaccount/killinary/controller/SessionControler.java
@@ -0,0 +1,27 @@
+package com.geraldsaccount.killinary.controller;
+
+import java.util.Set;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.geraldsaccount.killinary.model.dto.output.SessionSummaryDTO;
+import com.geraldsaccount.killinary.service.SessionService;
+
+@RestController
+@RequestMapping("/api/sessions")
+public class SessionControler {
+
+    private final SessionService sessionService;
+
+    public SessionControler(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping()
+    public Set<SessionSummaryDTO> getUsersEvents(Authentication authentication) {
+        return sessionService.getSessionSummariesFrom(authentication.getName());
+    }
+}
---
+++ backend/src/main/java/com/geraldsaccount/killinary/service/SessionService.java
@@ -0,0 +1,42 @@
+package com.geraldsaccount.killinary.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.geraldsaccount.killinary.model.dto.output.SessionSummaryDTO;
+import com.geraldsaccount.killinary.repository.SessionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SessionService {
+    private final SessionRepository sessionRepository;
+
+    public Set<SessionSummaryDTO> getSessionSummariesFrom(String oauthId) {
+        Set<SessionSummaryDTO> usersSessions = new HashSet<>();
+
+        sessionRepository.findAllByUserId(oauthId).stream()
+                .map(session -> {
+                    String characterName = session.getCharacterAssignments().stream()
+                            .filter(a -> a.getUser().getOauthId().equals(oauthId))
+                            .findFirst()
+                            .map(a -> a.getCharacter().getName())
+                            .orElse(null);
+
+                    return SessionSummaryDTO.builder()
+                            .sessionId(session.getId())
+                            .hostName(session.getHost().getFirstName())
+                            .storyName(session.getStory().getTitle())
+                            .assignedCharacterName(characterName)
+                            .sessionDate(session.getStartedAt())
+                            .build();
+                })
+                .forEach(usersSessions::add);
+
+        return usersSessions;
+    }
+
+}
