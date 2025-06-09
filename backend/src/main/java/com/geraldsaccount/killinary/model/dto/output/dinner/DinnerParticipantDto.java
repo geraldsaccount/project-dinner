c4bbfe165f11f97fde3d92b06a7b6533d9d0346f
gerald <geraldmontufar@gmail.com>
2025-06-09 17:32:14 +0200

    changed dto to send more details on characters

--- backend/src/main/java/com/geraldsaccount/killinary/model/dto/output/dinner/DinnerParticipantDto.java
+++ backend/src/main/java/com/geraldsaccount/killinary/model/dto/output/dinner/DinnerParticipantDto.java
@@ -1,10 +1,10 @@
 package com.geraldsaccount.killinary.model.dto.output.dinner;
 
-import com.geraldsaccount.killinary.model.dto.output.shared.CharacterSummaryDto;
+import com.geraldsaccount.killinary.model.dto.output.detail.CharacterDetailDto;
 import com.geraldsaccount.killinary.model.dto.output.shared.UserDto;
 
 public record DinnerParticipantDto(
         UserDto user,
-        CharacterSummaryDto character) {
+        CharacterDetailDto character) {
 
 }
