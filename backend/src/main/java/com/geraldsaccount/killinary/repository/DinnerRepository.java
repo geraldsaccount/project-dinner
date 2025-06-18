fcb1a3fd17a76548b3847932f7a87610d2cc0f92
gerald <geraldmontufar@gmail.com>
2025-06-18 09:11:35 +0200

    fixed dinner cards when host is not assigned to a character yet

--- backend/src/main/java/com/geraldsaccount/killinary/repository/DinnerRepository.java
+++ backend/src/main/java/com/geraldsaccount/killinary/repository/DinnerRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.geraldsaccount.killinary.model.dinner.Dinner;
 
 public interface DinnerRepository extends JpaRepository<Dinner, UUID> {
-    @Query("SELECT d FROM Dinner d JOIN d.participants u WHERE u.oauthId = :oauthId")
-    List<Dinner> findAllByUserId(String oauthId);
+	@Query("SELECT DISTINCT d FROM Dinner d LEFT JOIN d.participants u WHERE u.oauthId = :oauthId OR d.host.oauthId = :oauthId")
+	List<Dinner> findAllByUserId(String oauthId);
 }
