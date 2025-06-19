593578c94e9ea17e3bc6778655b8b3aad4f52e48
gerald <geraldmontufar@gmail.com>
2025-06-19 14:49:26 +0200

    changed editorcontroller to expect a multipart form so we can send multiple images next to the json

--- backend/src/main/java/com/geraldsaccount/killinary/controller/EditorController.java
+++ backend/src/main/java/com/geraldsaccount/killinary/controller/EditorController.java
@@ -1,11 +1,17 @@
 package com.geraldsaccount.killinary.controller;
 
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.geraldsaccount.killinary.exceptions.MysteryCreationException;
 import com.geraldsaccount.killinary.model.dto.input.create.CreateMysteryDto;
 import com.geraldsaccount.killinary.service.EditorService;
 
@@ -18,8 +24,11 @@ import lombok.RequiredArgsConstructor;
 public class EditorController {
     private final EditorService service;
 
-    @PostMapping()
-    public void createMystery(Authentication auth, @RequestBody CreateMysteryDto input) {
-        service.createMystery(input);
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public void createMystery(
+            Authentication auth,
+            @RequestPart("mystery") CreateMysteryDto mysteryDto,
+            @RequestParam(required = false) Map<String, MultipartFile> files) throws MysteryCreationException {
+        service.createMystery(mysteryDto, files);
     }
 }
